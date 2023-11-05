87a593f9433ce9af3c158ffa966581272675eee2
SeolDonghun <ssalssi1@gmail.com>
2023-11-05 10:21:31 +0900

    remove audio component, add debug feature for chat_history

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,6 @@ from dotenv import load_dotenv, find_dotenv
 
 import streamlit as st
 from llm_tools.prompts import humetro_system_prompt
-# from audiorecorder import AudioRecorder, audiorecorder
-# from gtts import gTTS
 
 from capturing_callback_handler import playback_callbacks
 from clear_results import with_clear_container
@@ -37,21 +35,28 @@ if 'langchain_messages' not in st.session_state:
     st.session_state.langchain_messages = []
 
 chat_history = StreamlitChatMessageHistory(key="langchain_messages")
-memory = ConversationBufferMemory(memory_key='history', chat_memory=chat_history)
+memory = ConversationBufferMemory(memory_key='chat_history', chat_memory=chat_history)
+
 
 if len(chat_history.messages)  == 0:
     chat_history.add_ai_message("안녕하세요 하단역의 인공지능 어시스턴트입니다. 궁금한 것이 있으시면 물어보세요!")
 
 st.write("# 🚇 Humetro AI Assistant")
 st.write("### 🤖 인공지능 어시스턴트에게 질문해보세요!")
-
-st.write(st.session_state)
+st.write('**이전 대화목록 확인하기(아래 클릭)**')
+st.json(st.session_state, expanded=False)
+st.write(chat_history.messages)
 
 output_container = st.empty()
 answer_container = st.empty()
 
 output_container = output_container.container()
-for msg in st.session_state.langchain_messages:
+# 최초 인삿말이 출력되도록 한다.
+if len(st.session_state.langchain_messages) == 1:
+    output_container.chat_message(chat_history.messages[0].type).write(chat_history.messages[0].content)
+
+# 중복을 배제하기 위해 마지막 두개의 메시지를 제외한 메시지를 출력한다.
+for msg in st.session_state.langchain_messages[:-2]:
     output_container.chat_message(msg.type).write(msg.content)
 
 if user_input := st.chat_input('이곳에 질문을 입력하세요.'):
@@ -60,6 +65,7 @@ if user_input := st.chat_input('이곳에 질문을 입력하세요.'):
     answer_container = output_container.chat_message("assistant")
     st_callback = StreamlitCallbackHandler(answer_container)
 
+    haa_executor.memory = memory
     answer = haa_executor.run({"input":user_input}, callbacks=[st_callback])
     answer_container.write(answer)
 
@@ -67,19 +73,3 @@ if user_input := st.chat_input('이곳에 질문을 입력하세요.'):
     chat_history.add_user_message(user_input)
     chat_history.add_ai_message(answer)
     save_to_mongo(user_input, answer)
-
-# if audio_input := st.audio_recorder("audio.wav"):
-#     output_container = output_container.container()
-#     output_container.chat_message("user").write(audio_input)
-
-#     answer_container = output_container.chat_message("assistant")
-#     st_callback = StreamlitCallbackHandler(answer_container)
-
-#     answer = haa.run(audio_input, callbacks=[st_callback])
-#     answer_container.write(answer)
-
-#     save_to_mongo(audio_input, answer)
-
-view_messages = st.expander("**모든 대화 목록  확인하기(클릭)**")
-with view_messages:
-    view_messages.json(st.session_state.langchain_messages)
