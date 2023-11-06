82830f59e2dc33b5ea05a33f94260bd0f64c1c6d
SeolDonghun <ssalssi1@gmail.com>
2023-11-06 15:16:44 +0900

    Refactor tools for openai function, function style

--- llm_tools/GoogleRoutes.py
+++ llm_tools/GoogleRoutes.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel, Field
-from typing import Optional, Type
-from langchain.tools import BaseTool
+# https://github.com/langchain-ai/langchain/issues/9441
+# use pydantic.v1
+from pydantic.v1 import BaseModel, Field
+from langchain.agents import tool
+from requests import Response
 
 import json
 import requests
@@ -9,20 +11,21 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file
 
-
-class GoogleRouteToolInputChecker(BaseModel):
-    """Input checker for GoogleRoute Tool"""
-    destination: str = Field(
-        ..., description="Destination to get directions from 하단역")
+base_location = os.environ['BASE_LOCATION']
 
 
-class GoogleRouteTool(BaseTool):
-    name = "GoogleRouteTool"
-    description = "Get directions from 하단역 to a given destination Always use this tool when answering questions about directions"
-    # TODO: 의존성 주입을 통해 다른 역에서도 출발할 수 있도록 하기
-    # 현재는 하단 기준으로 하드코딩되어 있음.
+class GoogleRoutesInput(BaseModel):
+    departure: str = Field(..., description="Departure to get directions if not specified you must use 하단역")
+    destination: str = Field(..., description="Destination to get directions")
 
-    def call_api(self, destination: str) -> str:
+@tool(args_schema=GoogleRoutesInput)
+def get_routes(departure: str, destination: str) -> str:
+    """Use Google Routes API to get directions from departure to destination"""
+
+    def get_departure( address: str) -> str:
+        return "부산광역시 사하구 낙동남로 지하1415 (하단동)"
+
+    def call_api(departure: str, destination: str) -> Response :
         endpoint = 'https://routes.googleapis.com/directions/v2:computeRoutes'
 
         headers = {
@@ -30,19 +33,20 @@ class GoogleRouteTool(BaseTool):
             'X-Goog-Api-Key': os.environ['GOOGLE_MAPS_KEY'],
             'X-Goog-FieldMask': 'routes.legs.steps.transitDetails',
         }
+        # departure_address = self.get_departure(address)
 
         payload = {
             "origin": {
-                "address": "부산광역시 사하구 낙동남로 지하1415 (하단동)"
+                "address": departure
             },
             "destination": {
                 "address": destination
             },
             "travelMode": "TRANSIT",
+            # "travelMode": "WALK",
             "computeAlternativeRoutes": False,
             "transitPreferences": {
-                "routingPreference": "LESS_WALKING",
-                "allowedTravelModes": ["SUBWAY"]
+                "routingPreference": "LESS_WALKING", #FEWER_TRANSFERS, LESS_WALKING
             },
             "languageCode": "ko-KR",
             "units": "METRIC"
@@ -51,25 +55,10 @@ class GoogleRouteTool(BaseTool):
                                  data=json.dumps(payload))
         return response
 
-    def _run(self, destination: str) -> str:
-        response = self.call_api(destination)
-        result_prefix = "<<HUMETRO_AI_DIRECTIONS>>\n"
-        if response.status_code != 200:
-            try:
-                error_msg = response.json()['error']['message']
-                return result_prefix + "ERROR : " + error_msg
-            except:
-                return result_prefix + "ERROR : " + "알 수 없는 오류가 발생했습니다."
-        if 'routes' not in response.json():
-            return result_prefix + "ERROR : " + "해당 경로를 찾지 못했습니다."
-        recommened_route = response.json()['routes'][0]['legs'][0]
-
-        return result_prefix + self.parse_route_to_string(destination, recommened_route)
-
-    def parse_route_to_string(self, destination: str, recommened_route: dict) -> str:
+    def parse_route_to_string(departure: str, destination: str, recommened_route: dict) -> str:
         steps = recommened_route['steps']
         order_num = 1
-        result = f"**하단역에서 {destination}까지 이동경로**\n"
+        result = f"**{departure}에서 {destination}까지 이동경로**\n"
         for step in steps:
             if not step:
                 continue
@@ -81,8 +70,8 @@ class GoogleRouteTool(BaseTool):
             stop_count = step['transitDetails']['stopCount']
             stop_placeholder = ''
             if transit_type == 'SUBWAY':
-                departure_stop = self.add_station_suffix(departure_stop)
-                arrival_stop = self.add_station_suffix(arrival_stop)
+                departure_stop = add_station_suffix(departure_stop)
+                arrival_stop = add_station_suffix(arrival_stop)
                 stop_placeholder = '역'
                 transit_line += '으'
             if transit_type == 'BUS':
@@ -96,7 +85,7 @@ class GoogleRouteTool(BaseTool):
 
         return result
 
-    def add_station_suffix(self, station: str) -> str:
+    def add_station_suffix(station: str) -> str:
         station = station.replace('경찰서', '')
 
         if station[-1] == '역':
@@ -104,12 +93,27 @@ class GoogleRouteTool(BaseTool):
         else:
             return station + '역'
 
-    args_schema: Optional[Type[BaseModel]] = GoogleRouteToolInputChecker
+    def run(departure: str, destination:str) -> str:
+        response = call_api(departure, destination)
+        result_prefix = "<<HUMETRO_AI_DIRECTIONS>>\n"
+        if response.status_code != 200:
+            try:
+                error_msg = response.json()['error']['message']
+                return result_prefix + "ERROR : " + error_msg
+            except:
+                return result_prefix + "ERROR : " + "알 수 없는 오류가 발생했습니다."
+        if 'routes' not in response.json():
+            return result_prefix + "ERROR : " + "해당 경로를 찾지 못했습니다."
+        recommened_route = response.json()['routes'][0]['legs'][0]
+
+        return result_prefix + parse_route_to_string(departure, destination, recommened_route)
+    
+    return run(departure, destination)
 
 
 if __name__ == "__main__":
     busan_tourist_spots = [
-        "영화의거리",
+        "큐병원",
         "태종대",
         "감천문화마을",
         "부산타워",
@@ -132,6 +136,4 @@ if __name__ == "__main__":
     ]
 
     for loc in busan_tourist_spots:
-        tool = GoogleRouteTool()
-        result = tool._run(loc)
-        print(result)
+        pass
--- llm_tools/HumetroWebSearch.py
+++ llm_tools/HumetroWebSearch.py
@@ -1,6 +1,7 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
+from typing import List, Any
 
 from duckduckgo_search import ddg
 from langchain.document_loaders import WebBaseLoader
@@ -8,10 +9,9 @@ from langchain.schema import Document
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.agents import tool
 
-from langchain.tools import BaseTool
-from typing import Optional, Type
-from pydantic import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())  # read local .env file
@@ -20,15 +20,13 @@ _ = load_dotenv(find_dotenv())  # read local .env file
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-class HumetroWebSearchInputChecker(BaseModel):
-    """Input for humetro web search check"""
+class HumetroWebSearchInput(BaseModel):
     query: str = Field(...,
-                       description="Query to search on http://www.humetro.busan.kr/")
+                       description="Query to search on http://www.humetro.busan.kr/. Query must be in KOREAN.")
 
-
-class HumetroWebSearchTool(BaseTool):
-    name = "get_humetro_web_search"
-    description = "If you don't find adequete tools, use this tool to Get the search result for a given query on http://www.humetro.busan.kr/"
+@tool(args_schema=HumetroWebSearchInput)
+def get_search(query: str) -> str:
+    """Use this tool get information that might changed from times. searches Humetro official website"""
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -39,36 +37,33 @@ class HumetroWebSearchTool(BaseTool):
         'Upgrade-Insecure-Requests': '1',
         'Cache-Control': 'max-age=0',
     }
-    docs = []
-    splits = []
-    vectordb: Chroma | None = None
+
     embedding = OpenAIEmbeddings()
 
-    def split_docs(self):
-        chunk_size = 1000
+    def split_docs(docs: List[Any]) -> List[Document]:
+        chunk_size = 2000
         chunk_overlap = 200
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=chunk_size,
             chunk_overlap=chunk_overlap)
-        self.splits = text_splitter.split_documents(self.docs)
+        return text_splitter.split_documents(docs)
 
-    def gen_vectordb(self):
-        self.vectordb = Chroma.from_documents(
-            documents=self.splits,
-            embedding=self.embedding,
-            persist_directory='./chroma',
+    def gen_vectordb(splits: List[Document]) -> Chroma:
+        vectordb = Chroma.from_documents(
+            documents=splits,
+            embedding=embedding,
+            persist_directory='./chroma/web', #chroma/web for web search, chroma/wiki for wiki search
         )
+        return vectordb
 
-    def query_vectordb(self, query):
-        docs = self.vectordb.similarity_search(query, k=3)
-        print('vectordb query results: ', docs)
-        return docs
 
-    def _run(self, query):
+    def run(query):
         prefix = "site:http://www.humetro.busan.kr/homepage/default/ "
         query = prefix + query
         search_results = [i['href'] for i in ddg(query, max_results=5)]
         target_urls = set()
+        docs = []
+        splits = []
 
         for url in search_results:
             if "main.do" in url or "login.do" in url:
@@ -76,23 +71,24 @@ class HumetroWebSearchTool(BaseTool):
             target_urls.add(url)
 
         for url in target_urls:
-            res = self.parse_response(url)
+            res = parse_response(url)
             if res:
-                self.docs.append(res)
+                docs.append(res)
 
-        if len(self.docs) == 0:
+        if len(docs) == 0:
             return ""
 
-        self.split_docs()
-        self.gen_vectordb()
-        result = self.query_vectordb(query)
+        splits = split_docs(docs)
+        vectordb = gen_vectordb(splits)
+        similar_docs = vectordb.similarity_search(query, k=3)
+        result = ""
+        for doc in similar_docs:
+            for key, value in doc.metadata.items():
+                result += f"{key}: {value}\n"
+            result += f"{doc.page_content}\n\n"
         return result
 
-    def _arun(self, query):
-        raise NotImplementedError(
-            "HumetroWebSearchTool does not support async")
-
-    def clean_whitespace(self, text):
+    def clean_whitespace(text):
         text = text.replace(
             "\n현재 열람하신 페이지의 내용이나 사용 편의성에 만족하십니까?\n\n\xa0\xa0매우만족(5점)\n\n\n\xa0\xa0만족(4점)\n\n\n\xa0\xa0보통(3점)\n\n\n\xa0\xa0불만족(2점)\n\n\n\xa0\xa0매우불만족(1점)\n\n\n평가하기\n",
             ""
@@ -105,8 +101,8 @@ class HumetroWebSearchTool(BaseTool):
         cleaned_text = '\n'.join(lines)
         return cleaned_text
 
-    def parse_response(self, url):
-        res = requests.get(url, headers=self.headers, verify=False)
+    def parse_response(url) -> Document | bool:
+        res = requests.get(url, headers=headers, verify=False)
         if res.status_code != 200:
             return False
 
@@ -118,7 +114,7 @@ class HumetroWebSearchTool(BaseTool):
         if content_tag is None:
             return False
         content_text = content_tag.text
-        content_text = self.clean_whitespace(content_text)
+        content_text = clean_whitespace(content_text)
 
         doc = Document(
             page_content=content_text,
@@ -130,9 +126,4 @@ class HumetroWebSearchTool(BaseTool):
         )
         return doc
 
-    args_schema: Optional[Type[BaseModel]] = HumetroWebSearchInputChecker
-
-
-if __name__ == "__main__":
-    tool = HumetroWebSearchTool()
-    tool.run("도시철도 견학정보")
+    return run(query)
