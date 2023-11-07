d1a1d8640019b29babdcba781aae46fe28f448ca
SeolDonghun <ssalssi1@gmail.com>
2023-11-07 10:33:07 +0900

    Ensemble retriever implemented

--- llm_tools/HumetroWikiSearch.py
+++ llm_tools/HumetroWikiSearch.py
@@ -1,6 +1,8 @@
 # 노션에 저장된 Humetro wiki를 대상으로 semantic search를 수행한다.
+import pickle
 
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
 from langchain.vectorstores import Chroma
 
 from langchain.tools import BaseTool
@@ -21,8 +23,22 @@ class HumetroWikiSearchInput(BaseModel):
 def get_wiki(query:str) -> str:
     """You should use this tool to answer following topics, 환승, 전화번호, 정기승차권, 다자녀 안내, 이중개표,  운임반환, 재개표 , 단체승차권, 부가운임, 현금영수증, 휴대금지, 금지행위, 캐시비 및 티머니 카드, 고객응대, 유실물, 영업배상, 물품보관함, 자전거, 봉사활동, 교통카드, 복자교통카드, 동백패스"""
     # TODO implement hybrid ensemble search
+
+# To load the list of documents from the binary file elsewhere
+    with open('documents_export.pkl', 'rb') as f:
+        loaded_documents = pickle.load(f)
+
+    bm25_retriever = BM25Retriever.from_documents(loaded_documents)
+    bm25_retriever.k = 2
+    
     embedding = OpenAIEmbeddings()
     vectordb = Chroma(persist_directory="./chroma/wiki", embedding_function=embedding)
-    result = vectordb.similarity_search(query, k=3)
-    print("wiki search result: ", result)
-    return result
+    chroma_retriever = vectordb.as_retriever(search_kwargs={"k":2})
+    
+    
+    ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, chroma_retriever], weights=[0.5, 0.5])  
+    result_docs = ensemble_retriever.get_relevant_documents(query)
+    return '\n'.join([doc.page_content for doc in result_docs])
+
+if __name__ == "__main__":
+    get_wiki('보관함')
